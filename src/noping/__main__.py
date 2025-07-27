44085880c201f807d37bc35a6bd96696be583406
twonfi <git@twonum.org>
2025-07-26 19:39:22 -0700

    Use `dotenv`

--- src/noping/__main__.py
+++ src/noping/__main__.py
@@ -1,7 +1,10 @@
 import os
 
+from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+load_dotenv()
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
